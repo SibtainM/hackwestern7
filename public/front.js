4a8e565b8330d5f7eee57aea89275ebad7a7d42f
Sibtain Manesiya <smanesiy@uwo.ca>
2020-11-21 00:14:36 -0500

    Created Tempate Function to Populate PDF
    Created the function which will be called to dynamically populate the web activity history report (PDF).

--- public/front.js
+++ public/front.js
@@ -2,6 +2,20 @@ const { jsPDF } = require("jspdf");
 // will automatically load the node version
 const pdf = new jsPDF();
 
+var x = 0;
+var y = 0;
+
+//call this function whenever we need to add a new entry from the browswer history.
+function pdfPopulator(history){
+
+    //Copy in the data of the new web history entry onto the PDF 
+
+    //change the x and y coordinates for the next extry
+    x = x + 10;
+    y = y + 10;
+}
+
+
 function createPDF(){
 
     let fname = document.getElementById("firstname").value;
@@ -19,5 +33,10 @@ function createPDF(){
     pdf.text(idnum, 10, 40)
 
     pdf.save(fname.charAt(0) + "_" + lname + idnum);
+
+    for (let index = 0; index < window.history.length; index++) {
+        //pdfPopulator(window.history);
+        
+    }
     
 }
