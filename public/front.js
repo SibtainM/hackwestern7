d2a36f399bbea846e7698a238a774f0343a6efad
Hanniya Zohdi <hanniyazohdi@MacBook-Pro.local>
2020-11-20 21:00:29 -0800

    Implemented jsPDF Library
    Created basic function that exports user's full name and student ID number to downloaded PDF.

--- public/front.js
+++ public/front.js
@@ -1,1 +1,23 @@
-//Add Front End Code Here
+const { jsPDF } = require("jspdf"); 
+// will automatically load the node version
+const pdf = new jsPDF();
+
+function createPDF(){
+
+    let fname = document.getElementById("firstname").value;
+    let lname = document.getElementById("lastname").value;
+    let idnum = document.getElementById("idnumber").value;
+
+    pdf.setFont("times", "bold");
+    pdf.setFontSize(14);
+    pdf.text("Exam History Log", 10, 20);
+
+    pdf.setFont("times", "regular");
+    pdf.setFontSize(12);
+    pdf.text(lname + ", ", 10, 30)
+    pdf.text(fname, 30, 30)
+    pdf.text(idnum, 10, 40)
+
+    pdf.save(fname.charAt(0) + "_" + lname + idnum);
+    
+}
